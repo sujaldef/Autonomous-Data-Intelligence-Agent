f719a13b3efa7019874264b9ecb2933e0b0ef602
Sujal Koshta <sujalkoshtawork@gmail.com>
2026-04-30 00:54:14 +0530

    initialized frontend structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen text-2xl font-bold">
+      hello world
+    </div>
+  )
+}
+
+export default App
+
